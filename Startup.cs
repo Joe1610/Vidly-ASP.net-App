5bdb0230bacd93af5c9c50195cf7bb3654216d7e
Joe1610 <joemohammed157@gmail.com>
2021-10-09 16:25:59 +0200

    Display the list of movies

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vidly.Data;
 
 namespace Vidly
 {
@@ -23,6 +25,10 @@ namespace Vidly
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")
+                ));
             services.AddRazorPages();
         }
 
@@ -50,7 +56,12 @@ namespace Vidly
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-            });
+ 
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
         }
     }
 }
