58980f8183681fdd16c23bae05d559cdaa5fb6d1
Joe1610 <joemohammed157@gmail.com>
2021-10-09 16:23:00 +0200

    Display the list of customers

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using Controller = Microsoft.AspNetCore.Mvc.Controller;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+
+        }
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return NotFound();
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+           { new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
