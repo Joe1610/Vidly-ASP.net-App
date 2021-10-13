87f273149f954609e01e8bd94b517d098135b96d
Joe1610 <joemohammed157@gmail.com>
2021-10-13 16:50:55 +0200

    Add a form to add/update a movie

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -27,6 +27,47 @@ namespace Vidly.Controllers
             return View(movies);
 
         }
+        public IActionResult New()
+        {
+            var genres = _context.Genre.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genre = genres
+            };
+            return View("MovieForm", viewModel);
+        }
+        [HttpPost]
+        public IActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+          
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+        public IActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return NotFound();
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genre = _context.Genre.ToList(),
+            };
+            return View("MovieForm", viewModel);
+        }
         public IActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,10 +12,13 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
         public Genre Genre { get; set; }
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
+        [Display( Name = "Number in Stock")]
         public int NumberInStock { get; set; }
     }
 }
